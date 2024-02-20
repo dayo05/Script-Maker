5b13a58c842aea6680b9db8de1c6891478f64564
Dayo <dayo05@daum.net>
2024-02-20 16:32:39 +0900

    Add ability for zoom in-out

--- Assets/ScriptMaker/Program/Data/Option.cs
+++ Assets/ScriptMaker/Program/Data/Option.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using JetBrains.Annotations;
 
 namespace ScriptMaker.Program.Data
 {   
--- Assets/ScriptMaker/Program/MainCameraCS.cs
+++ Assets/ScriptMaker/Program/MainCameraCS.cs
@@ -1,21 +1,41 @@
+using System;
 using UnityEngine;
 
 namespace ScriptMaker.Program
 {
     public class MainCameraCS : MonoBehaviour
     {
+        private RectTransform MainCanvasTransform;
+
+        private void Start()
+        {
+            MainCanvasTransform = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        }
+
+        private float ScaleBias => 30 * Time.deltaTime;
+
         void Update()
         {
+            if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.LeftCommand)) && Input.mouseScrollDelta.normalized != Vector2.zero)
+            {
+                var ls = MainCanvasTransform.localScale;
+                ls.Scale(Vector2.one * Mathf.Pow(1.1f, Input.mouseScrollDelta.y));
+                if (ls.x > 0.15) ls = new Vector3(0.15f, 0.15f, 0.15f); // Clamp
+                else if (ls.x < 0.001) ls = new Vector3(0.001f, 0.001f, 0.001f);
+                MainCanvasTransform.localScale = ls;
+            }
+            else transform.Translate(Input.mouseScrollDelta * new Vector2(-1, 9.0f / 16));
+
             if (EditorMain.IsIgnoreSelectionMod) return;
-            
+
             if (Input.GetKey(KeyCode.A))
-                transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime);
+                transform.Translate(new Vector3(-1, 0, 0) * ScaleBias);
             if (Input.GetKey(KeyCode.S))
-                transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime);
+                transform.Translate(new Vector3(0, -1, 0) * ScaleBias);
             if (Input.GetKey(KeyCode.D))
-                transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+                transform.Translate(new Vector3(1, 0, 0) * ScaleBias);
             if (Input.GetKey(KeyCode.W))
-                transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime);
+                transform.Translate(new Vector3(0, 1, 0) * ScaleBias);
         }
     }
 }
